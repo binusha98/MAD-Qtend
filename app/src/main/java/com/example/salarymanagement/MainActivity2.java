b7b0cd37b8ae368aaafd73422a1dc21da02e6679
binusha98 <IT20479114@my.sliit.lk>
2021-08-15 17:24:43 +0530

    Qtend

---
+++ app/src/main/java/com/example/salarymanagement/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.salarymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button button3;
+    Button button4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this,MainActivity3.class));
+            }
+        });
+
+        button3 = findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this,MainActivity.class));
+            }
+        });
+    }
+}
